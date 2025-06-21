7de3e3a42843fc26f4651ddc63a72bf9a90a75e9
alfkristian02 <alfkristian02@gmail.com>
2025-06-22 00:20:21 +0200

    feat: word appearing in at the top of the page in random position

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,45 @@
 <script>
+
+    import { onMount, onDestroy } from 'svelte';
+    import { fade } from 'svelte/transition';
+
+    let up_time = 0;
+
+    let clientWidth = 0;
+
+    onMount(() =>{
+        let mount_time = Date.now();
+
+        setInterval(() => {
+            up_time = Math.floor((Date.now() - mount_time) / 1000);
+        })
+    });
+
+    onDestroy(() => {
+        up_time = 0;
+    });
+
+    // This is probably going to be a list of words later, imported from some file. 
+    let words = ["one", "two", "three", "four"]
 </script>
 
-<h1>Less goo</h1>
+
+<div id="container" bind:clientWidth>
+    {#each words as word, i}
+        {#if i <= up_time}
+            <p style="left: {Math.floor(Math.random() * clientWidth)}px;">{word}</p>
+            <!-- {console.log(clientWidth)} -->
+        {/if}
+    {/each}
+</div>
+
+<style>
+    #container {
+        display: flex;
+        width: auto;
+    }
+
+    p {
+        position: absolute;
+    }
+</style>
