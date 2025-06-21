8f0963af13134d12df171f0724c60b016fe2c6eb
alfkristian02 <alfkristian02@gmail.com>
2025-06-22 00:51:57 +0200

    refactor: made appearing word its own component

---
+++ src/lib/components/Word.svelte
@@ -0,0 +1,15 @@
+<script>
+
+    export let availableWidth;
+    export let word;
+
+</script>
+
+<p style="left: {Math.floor(Math.random() * availableWidth)}px;">{word}</p>
+
+<style>
+    p{
+        position: absolute;
+        font-size: 30px;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,34 +1,35 @@
 <script>
 
     import { onMount, onDestroy } from 'svelte';
-    import { fade } from 'svelte/transition';
+
+    import Word from '$lib/components/Word.svelte';
 
     let up_time = 0;
-
-    let clientWidth = 0;
+    let clientWidth;
+    let interval ;
 
     onMount(() =>{
         let mount_time = Date.now();
 
-        setInterval(() => {
+        interval = setInterval(() => {
             up_time = Math.floor((Date.now() - mount_time) / 1000);
         })
     });
 
     onDestroy(() => {
+        clearInterval(interval)
         up_time = 0;
     });
 
     // This is probably going to be a list of words later, imported from some file. 
-    let words = ["one", "two", "three", "four"]
+    let words = ["yoo", "yoo", "yoo", "yoo", "yoo", "yoo", "yoo"]
 </script>
 
 
 <div id="container" bind:clientWidth>
     {#each words as word, i}
         {#if i <= up_time}
-            <p style="left: {Math.floor(Math.random() * clientWidth)}px;">{word}</p>
-            <!-- {console.log(clientWidth)} -->
+            <Word {word} availableWidth={clientWidth} />
         {/if}
     {/each}
 </div>
@@ -37,9 +38,5 @@
     #container {
         display: flex;
         width: auto;
-    }
-
-    p {
-        position: absolute;
     }
 </style>
