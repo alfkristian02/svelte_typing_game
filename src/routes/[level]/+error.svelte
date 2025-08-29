5124284ccaa5647b5cbf629b8dce64d9ad1bbb21
alfkristian02 <alfkristian02@gmail.com>
2025-08-29 20:25:55 +0200

    feat: error page for wrong route on level

---
+++ src/routes/[level]/+error.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import { page } from '$app/state';
+</script>
+
+<h1>{page.status}: {page.error.message}</h1>
+
+<style>
+	h1 {
+		text-align: center;
+		margin-top: 45vh;
+	}
+</style>
--- src/routes/[level]/+page.server.js
+++ src/routes/[level]/+page.server.js
@@ -1,11 +1,15 @@
 import { levels } from '$lib/data/levels';
+import { error } from '@sveltejs/kit';
 
 export function load({ params }) {
-    const level = levels.find((level) => level.route === params.level)
+	const level = levels.find((level) => level.route === params.level);
 
-    // need some error checking or something i guess
+	// need some error checking or something i guess
+	if (!level) {
+		throw error(404, `Level does not exist`);
+	}
 
-    return {
-        level
-    };
+	return {
+		level
+	};
 }
