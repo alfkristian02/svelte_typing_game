e28fed42f7c584c3a8da4d5db3321e739eb04b18
alfkristian02 <alfkristian02@gmail.com>
2025-09-15 00:25:51 +0200

    fix: unexpected behaviour in regards to the ordering. change it so a copy of the origional array is manipulated

--- src/lib/utils/fisher-yates_shuffle.ts
+++ src/lib/utils/fisher-yates_shuffle.ts
@@ -3,10 +3,12 @@ function get_random_number(min: number, max: number): number {
 }
 
 export function fisher_yates_shuffle<T>(sequence: Array<T>){
-	for (let i = sequence.length - 1; i > 0; i--) {
+	const copy = [...sequence];
+	for (let i = copy.length - 1; i > 0; i--) {
 		let j = get_random_number(0, i);
-		let temp = sequence[i];
-		sequence[i] = sequence[j];
-        sequence[j] = temp;
+		let temp = copy[i];
+		copy[i] = copy[j];
+        copy[j] = temp;
 	}
+	return copy;
 }
--- src/routes/[level]/+page.svelte
+++ src/routes/[level]/+page.svelte
@@ -1,15 +1,9 @@
 <script>
 	import Level from '$lib/components/Level.svelte';
 	import { fisher_yates_shuffle } from '$lib/utils/fisher-yates_shuffle';
-	import { onMount } from 'svelte';
 
     let { data } = $props();
 
-    let word_list = data.level?.words
-
-    onMount(() => {
-        fisher_yates_shuffle(word_list);
-    })
 </script>
 
-<Level {word_list} word_drop_interval={data.level?.word_drop_interval}/>
+<Level word_list={fisher_yates_shuffle(data.level?.words)} word_drop_interval={data.level?.word_drop_interval}/>
